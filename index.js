8b0826b0c29b48fe755c8e68a7ca7d8b54d5cdcb
Bellavely <bella.vaisborg@cs.colman.ac.il>
2023-03-12 09:53:38 +0200

    intial commit

--- index.js
+++ index.js
@@ -20,9 +20,11 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello",  (req, res) => {
+  res.send(Date());
 });
+
+
 
 
 
